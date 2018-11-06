c16e9fea79155bd704fd580c5f11a9fec16cfef6
robin <robinchow8991@gmail.com>
2018-11-07 00:36:40 +0800

    init repo

---
+++ comservfacade/src/main/java/pers/robin/micsd/comservfacade/ComservfacadeApplication.java
@@ -0,0 +1,12 @@
+package pers.robin.micsd.comservfacade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComservfacadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComservfacadeApplication.class, args);
+    }
+}
---
+++ comservfacade/src/test/java/pers/robin/micsd/comservfacade/ComservfacadeApplicationTests.java
@@ -0,0 +1,16 @@
+package pers.robin.micsd.comservfacade;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComservfacadeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
